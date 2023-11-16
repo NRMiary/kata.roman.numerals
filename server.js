db67d13e380b980cc6a0c897c066fce5171f6ea9
MiaryRa <miaryram@yahoo.com>
2023-11-16 23:30:50 +0100

    replace letter with number in arabic input

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors'); // Import the cors module
+
+// Configure CORS to allow all origins (for demonstration purposes, for local development)
+app.use(cors());
 
 app.use(express.json());
 
-// API endpoint to convert roman numerals to numeric values
+// API endpoint to convert Roman numerals to numeric values
 app.get('/api/convert-roman', (req, res) => {
     const romanNumeral = req.query.roman;
     if (typeof romanNumeral === 'string' && romanNumeral.length > 0) {
@@ -15,7 +19,7 @@ app.get('/api/convert-roman', (req, res) => {
     }
 });
 
-// API endpoint to convert arabic numbers to roman numerals
+// API endpoint to convert Arabic numbers to Roman numerals
 app.get('/api/convert-arabic', (req, res) => {
     const arabicNumber = req.query.arabic;
     if (isNumeric(arabicNumber)) {
@@ -32,7 +36,6 @@ app.listen(port, () => {
 
 // Function to convert Roman numerals to numeric values
 function convertRomanToNumeric(roman) {
-    // Define an object to map Roman symbols to their numeric values
     const romanNumerals = {
         I: 1,
         V: 5,
@@ -77,6 +80,9 @@ function convertArabicToRoman(arabic) {
     };
 
     let roman = "";
+
+    // Replace the letter "O" with "0" in the Arabic input
+    arabic = arabic.replace(/o/gi, '0');
 
     const sortedValues = Object.keys(romanNumerals)
         .map(Number)
