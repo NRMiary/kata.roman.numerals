91c418772b1e374496030f982b216a5f29aaa4d8
MiaryRa <miaryram@yahoo.com>
2023-12-17 09:12:48 +0100

    clean code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 // Object to store conversion results
 const conversionCache = {};
 
+let apiUrl;
+
+if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+    // In local development mode
+    apiUrl = 'http://localhost:3000'; 
+} else {
+    // In production mode
+    apiUrl = 'https://ws-roman-numerals-c84c6a63b965.herokuapp.com'; 
+}
 // Function to replace "O" or "o" with "0" in the input
 function replaceOWithZero(input) {
     return input.replace(/o/gi, '0');
@@ -11,25 +20,23 @@ function isArabicNumber(input) {
     // First call the replacement function
     const cleanedInput = replaceOWithZero(input);
 
-    // Check if the input is a valid Arabic number
     return /^[0-9]+$/.test(cleanedInput);
 }
 
 // Function to check if the input is a valid Roman numeral
 function isRomanNumber(input) {
-    // Check if the input is a valid Roman numeral
     const romanPattern = /^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/;
 
     return romanPattern.test(input);
 }
 
-let apiUrl;
-if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
-    // In local development mode
-    apiUrl = 'http://localhost:3000'; // Replace with the URL of your local API
-} else {
-    // In production mode
-    apiUrl = 'https://ws-roman-numerals-c84c6a63b965.herokuapp.com'; // Use the environment variable for the production URL
+// Function to update the HTML element with the result
+function updateResultOnUI(result) {
+    // Select the result element
+    let resultElement = document.getElementById('resultContainer');
+
+    // Update the content of the element with the result
+    resultElement.textContent = `Résultat: ${result}`;
 }
 
 // Function to perform the conversion by calling the REST API
@@ -47,9 +54,9 @@ function performConversion(input, conversionType) {
     if (conversionType === "arabicToRoman") {
         // Replace the letter "O" with "0" in the Arabic input
         input           = input.replace(/o/gi, '0');
-        apiUrlWithParam = `${apiUrl}/api/convert-arabic?arabic=${input}`; // Use the appropriate URL for Arabic to Roman conversion
+        apiUrlWithParam = `${apiUrl}/api/convert-arabic?arabic=${input}`; 
     } else {
-        apiUrlWithParam = `${apiUrl}/api/convert-roman?roman=${input}`; // Your API URL for Roman to Arabic conversion
+        apiUrlWithParam = `${apiUrl}/api/convert-roman?roman=${input}`; 
     }
 
     // Build the request object
@@ -85,22 +92,13 @@ function performConversion(input, conversionType) {
         })
         .catch((error) => {
             console.error('Error in API request:', error);
-            throw error; // Propagate the error to handle it later if needed
+            throw error; 
         });
-}
-
-// Function to update the HTML element with the result
-function updateResultOnUI(result) {
-    // Select the result element
-    let resultElement = document.getElementById('resultContainer');
-
-    // Update the content of the element with the result
-    resultElement.textContent = `Résultat: ${result}`;
 }
 
 // Function to handle the conversion when the button is clicked
 function handleConversion() {
-    let inputNumber = document.getElementById('inputNumber').value;
+    let inputNumber    = document.getElementById('inputNumber').value;
     let conversionType = document.getElementById('conversionType').value;
 
     if (conversionType === "romanToArabic" || conversionType === "arabicToRoman") {
@@ -118,6 +116,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let convertButton = document.getElementById('convertButton');
     convertButton.addEventListener('click', handleConversion);
 });
+
 
 module.exports = {
     replaceOWithZero,
